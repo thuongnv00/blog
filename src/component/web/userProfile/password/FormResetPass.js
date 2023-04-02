617bec3cd4db7cd4c8e399da787b54e641adadc6
thuongnv00 <ngovanthuong12369@gmail.com>
2023-04-02 13:57:54 +0700

    cm

--- src/component/web/userProfile/password/FormResetPass.js
+++ src/component/web/userProfile/password/FormResetPass.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import { Alert, AlertTitle, Box, Button, Container, LinearProgress, Paper, Snackbar, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
 import { event } from "jquery";
 export default function FormResetPass() {
 
-    const[newPass,setNewPass] = useState('')
-    const [confirmPass,setConfirmPass] = useState('')
+    const [newPass, setNewPass] = useState('')
+    const [confirmPass, setConfirmPass] = useState('')
+    const[success,setSuccess] = useState(false)
+    const [fail,setFail] = useState(false)
+    const [loading,setLoading] = useState(false)
+    const [data,setData] = useState('')
 
-    const onChange =(event)=> {
-        if(event.target.name === 'newPass') {
+    const onChange = (event) => {
+        if (event.target.name === 'newPass') {
             setNewPass(event.target.value)
         }
-        if(event.target.name === 'confirmPass') {
+        if (event.target.name === 'confirmPass') {
             setConfirmPass(event.target.value)
         }
     }
 
+    const onSuccess = ()=> {
+        setSuccess(false)
+    }
+    const onFail = ()=> {
+        setFail(false)
+    }
+
 
     const resetPass = () => {
+        setLoading(true);
+        setFail(false);
+        setSuccess(false);
         let data = JSON.stringify({
             "newPassword": newPass,
             "confirmPassword": confirmPass
@@ -37,14 +51,40 @@ export default function FormResetPass() {
 
         axios.request(config)
             .then((response) => {
+                setSuccess(true);
+                setLoading(false);
                 console.log(JSON.stringify(response.data));
+                setData(response.data.message)
             })
             .catch((error) => {
+                setFail(true);
+                setLoading(false)
                 console.log(error);
+                setData(error.response.data.message)
             });
     }
     return (<>
         <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Snackbar open={success} autoHideDuration={4000}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                onClose={onSuccess}>
+                <Alert onClose={onSuccess} severity="success" sx={{ width: '100%' }}>
+                    <AlertTitle><strong>Success</strong> </AlertTitle>
+                    Cập nhật mật khẩu thành công!
+
+                </Alert>
+            </Snackbar>
+
+            <Snackbar open={fail} autoHideDuration={4000}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                onClose={onFail}>
+                <Alert onClose={onFail} severity="error" sx={{ width: '100%' }}>
+                    <AlertTitle><strong>Lỗi xảy ra!</strong> </AlertTitle>
+                    {data}
+
+                </Alert>
+            </Snackbar>
+
             <Box sx={{ width: '500px', height: '50vh' }}>
                 <Paper align='center' sx={{ boxShadow: 'none' }}>
                     <Link to="/">
@@ -66,9 +106,6 @@ export default function FormResetPass() {
                         </div>
                         <div><TextField name="newPass" variant="outlined" size="small" placeholder="" onChange={onChange} fullWidth></TextField></div>
                     </div>
-                    <p>{newPass}</p>
-                    <p>{confirmPass}</p>
-
                     <div style={{ paddingTop: '20px' }}>
                         <div style={{ fontSize: '14px', paddingBottom: '5px' }}>
                             <span style={{ paddingRight: '5px', color: 'red' }}>*</span>
@@ -81,6 +118,12 @@ export default function FormResetPass() {
                     <div style={{ padding: '10px 0', display: 'flex', justifyContent: 'flex-end' }}>
                         <Button size="small" variant="contained" sx={{ textTransform: 'initial' }} onClick={resetPass}>Thay đổi mật khẩu</Button>
                     </div>
+
+                    {(loading) && <Box>
+                        <LinearProgress />
+                        <div><p style={{ marginTop: '5px', fontSize: '12px' }}>Sending...</p></div>
+                    </Box>
+                    }
 
 
                 </Paper>
